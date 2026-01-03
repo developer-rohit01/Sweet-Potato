7c6d0bde95d4fb6538ad15f6fcfff5a911b7239c
Rohit <rr5450534@gmail.com>
2026-01-03 19:48:23 +0530

    Add the code of area of circle using Function

---
+++ CircleArea.c
@@ -0,0 +1,24 @@
+ #define PI 3.14
+#include<stdio.h>
+
+
+float circle(int R){
+ float Area=PI*R*R;
+
+ return Area;
+}
+
+
+
+int main(){
+      float C,S;
+      printf("Enter the radius = ");
+      scanf("%f",&C);
+
+      S=circle(C);
+      printf("Area of  y cirlce is = %.2f",S);
+
+
+
+   return 0;
+}
