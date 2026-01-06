3f09229bec60e28a308c04a3c5ad1a32de9a5281
Rohit <rr5450534@gmail.com>
2026-01-06 23:29:34 +0530

    Add File Handling concept

---
+++ FileHandling.c
@@ -0,0 +1,30 @@
+ #include<stdio.h>
+ 
+ int main(){
+      //fil is a stream of bytes os use the byte as file 
+     //work for the file already exists 
+     //rb = read binary , wb= write binary
+     
+     FILE *src = fopen("meta.txt", "rb");
+    FILE *dest = fopen("zeta.txt", "wb");
+
+    if (src == NULL || dest == NULL) {
+        printf("File open error\n");
+        return 1;
+    }
+
+    char buffer[8];          // pick any size
+    size_t n;
+
+    do {
+        n = fread(buffer, 1, sizeof buffer, src);   // read up to buffer size
+        fwrite(buffer, 1, n, dest);                 // write ONLY what we read
+    } while (n>0);                                 // stop when fread returns 0
+
+    fclose(src);
+    fclose(dest);
+
+    
+   return 0;
+ }
+    
