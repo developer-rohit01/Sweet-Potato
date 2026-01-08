612e1be486a72f405c5aedb6b03c0d07ed4d0cb9
Rohit <rr5450534@gmail.com>
2026-01-08 22:28:14 +0530

    Creating the string base calculator

---
+++ simplecalculator.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+//trying to add this file on the git
+
+int main(){
+     float a,b;
+     char ch;
+
+      
+     printf("Enter The First value: ");
+     scanf("%f",&a);
+     printf("Enter the Second value: ");
+     scanf("%f",&b);
+     printf("Choose the Operator '+' '-' '*' '/' \n ");
+     scanf(" %c",&ch);
+2
+    
+     if(ch=='+'){
+        printf("%.1f + %.1f = %.1f",a,b,a+b);
+     }
+     
+     else if(ch=='-'){
+        printf("%.1f - %.1f = %.1f",a,b,a-b);
+     }
+     else if(ch=='*'){
+        printf("%.1f * %.1f = %.1f",a,b,a*b);
+     }
+     else if(ch=='/'){
+        printf("%.1f / %.1f = %.1f",a,b,a/b);
+     }
+     else {
+        printf("Invalid Opeator Selected");
+     }
+
+    getch();
+
+     return 0;
+     
+}
