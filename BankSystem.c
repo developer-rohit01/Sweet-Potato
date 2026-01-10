460a8691c114a7d1314c27dbdb46640c15516a48
Rohit <rr5450534@gmail.com>
2026-01-10 22:10:40 +0530

    Add the functionality in the project

--- BankSystem.c
+++ BankSystem.c
@@ -10,3 +10,46 @@ struct Account {
 
 
 
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Account {
+    int accNo;
+    char name[50];
+    float balance;
+};
+
+void createAccount();
+void depositMoney();
+void withdrawMoney();
+void checkBalance();
+void displayAll();
+
+int main() {
+    int choice;
+
+    do {
+        printf("\n--- Simple Banking System ---\n");
+        printf("1. Create Account\n");
+        printf("2. Deposit Money\n");
+        printf("3. Withdraw Money\n");
+        printf("4. Check Balance\n");
+        printf("5. Display All Accounts\n");
+        printf("6. Exit\n");
+
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1: createAccount(); break;
+            case 2: depositMoney(); break;
+            case 3: withdrawMoney(); break;
+            case 4: checkBalance(); break;
+            case 5: displayAll(); break;
+            case 6: exit(0);
+            default: printf("Invalid choice!\n");
+        }
+    } while(1);
+
+    return 0;
+}
