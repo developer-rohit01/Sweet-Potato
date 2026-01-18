8c67833842e3372b7f2b7639f2652a6ede1e9bc7
Rohit <rr5450534@gmail.com>
2026-01-18 23:00:55 +0530

    Add Initiation the Master recursion Code

---
+++ MasterFact.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int factorial(int n)
+{
+    if (n == 0)
+        return 1;
+
+    return n * factorial(n - 1);
+}
+
+
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("\nFactorial of %d = %d", num, factorial(num));
+
+  
+    return 0;
+}
