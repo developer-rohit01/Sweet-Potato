f1e1ffe1b2ad6dab4930a12682298c43a6c48ab2
Rohit <rr5450534@gmail.com>
2026-01-19 21:55:52 +0530

    Add Book Adding Code

--- LibraryManagement.c
+++ LibraryManagement.c
@@ -6,4 +6,35 @@ struct Book {
     int quantity;
 };
 
+void addBook()
+{
+    struct Book b;
+    FILE *fp;
+
+    fp = fopen("library.dat", "ab");
+
+    if (fp == NULL) {
+        printf("File cannot be opened.\n");
+        return;
+    }
+
+    printf("\nEnter Book ID: ");
+    scanf("%d", &b.bookId);
+
+    printf("Enter Book Title: ");
+    scanf(" %[^\n]", b.title);
+
+    printf("Enter Author Name: ");
+    scanf(" %[^\n]", b.author);
+
+    printf("Enter Quantity: ");
+    scanf("%d", &b.quantity);
+
+    fwrite(&b, sizeof(b), 1, fp);
+
+    fclose(fp);
+
+    printf("\nBook added successfully.\n");
+}
+
 
