21aebd50ab706c56caf53b1140c8fd81e65cadf2
Rohit <rr5450534@gmail.com>
2026-01-20 20:39:13 +0530

    Update this code for more readibilty

--- Occurrence.c
+++ Occurrence.c
@@ -4,7 +4,7 @@ void Occur(int a,int b);
 
 int main(){
   int a,b;
-
+  printf("********** Occurrence of a digit in a number ASCAT Question **********\n");
   printf("Enter the number you want to find its count: \a");
   scanf("%d",&a);
 
@@ -12,6 +12,12 @@ int main(){
   scanf("%d",&b);
   
   Occur(a,b);
+  
+  printf("\nPress Enter to exit...");
+  getchar();
+
+  getchar();
+
   return 0;
 }
 
@@ -25,4 +31,5 @@ void Occur(int a, int b){
     }
     b = b / 10;
   }
+  printf("The digit %d has occurred %d times in the given number.\n", a, count);
 }
