a725ddd177cfe6f70de3c15c313999cd1bbfc6c7
Rohit <rr5450534@gmail.com>
2026-01-23 22:28:06 +0530

    Add the givedate function

--- StructcPractice.c
+++ StructcPractice.c
@@ -7,19 +7,21 @@ struct date{
     int month;
     int year;
 };
+struct date givedate();
 
 int main(){
-
-  
   struct  date d1;
-   printf("Enter day momth year: ");
-   scanf("%d%d%d", &d1.day, &d1.month, &d1.year);
+
+  d1=givedate();
 
    printf("\n--- Date Details ---\n");
    printf("Date: %d-%d-%d\n", d1.day, d1.month, d1.year);
-
-
-
    return 0;
 }
 
+
+struct date givedate(){
+  struct date s1;
+  printf("Enter day month year: ");
+   scanf("%d%d%d", &s1.day, &s1.month, &s1.year);
+}
