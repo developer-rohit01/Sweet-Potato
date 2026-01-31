6a1138fadaee817b672c0e6a602d16d8fb34c2f1
Rohit <rr5450534@gmail.com>
2026-02-01 03:00:25 +0530

    Add iFelse Code

---
+++ iFeLse.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+  int a=5,b=2;
+
+  if(a>b){
+    printf("A is greater\n");
+  }
+  if(a>b){
+    printf("AA is a greater\n");
+  }
+  else if(a>b){
+    printf("Satwik\n");
+  }
+
+
+
+  // if(a>b){
+  //   printf("Both are equal");
+  // }
+  else{
+    printf("B is greater");
+  }
+   return 0;
+}
