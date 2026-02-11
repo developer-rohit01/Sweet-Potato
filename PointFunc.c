5959435df8f1d09f3fa1ad4ff04c4d6bb5d6a6fd
Rohit <rr5450534@gmail.com>
2026-02-11 23:40:56 +0530

    Add the user take value and revise the code

--- PointFunc.c
+++ PointFunc.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
+
+// Function prototype for swapping two numbers using pointers
 void swap(int *p,int *q);
+
+
 int main(){
-    int a=6,b=7;
+    int a,b;
+    printf("Enter the value of a and b:\n");
+    scanf("%d\n%d",&a,&b);
 
     swap(&a,&b);
     printf("The value of a is %d\n",a);
@@ -10,6 +16,7 @@ int main(){
    return 0;
 }
 
+// Function to swap two numbers using pointers
 void swap(int *p,int *q){
     int temp;
     temp = *p;
