0a3acb7536ba4d75faf3b7e3d0feeef6de270121
Rohit <rr5450534@gmail.com>
2026-06-09 22:08:45 +0530

    Add Assignment 01-ETE java Code

---
+++ Assignment_01_sem01.java
@@ -0,0 +1,26 @@
+
+//  Write a Java program to demonstrate data types, operators, control statements, and arrays.
+
+
+public class Assignment_01_sem01 {
+public static void main(String[] args) {
+int a = 10, b = 5;
+// Operators
+System.out.println("Addition = " + (a + b));
+System.out.println("Multiplication = " + (a * b));
+// Control Statement
+if(a > b)
+System.out.println("a is greater");
+// Loop
+for(int i = 1; i <= 5; i++)
+System.out.print(i + " ");
+// Array
+int arr[] = {10,20,30,40};
+System.out.println("\nArray Elements:");
+for(int x : arr)
+System.out.println(x);
+}
+}
+
+  
+
